83e94b76b149b87ee3ea5cc72d1f909a4977a25d
Hemang Seth <Hemang.Seth@iiitb.ac.in>
2025-11-12 21:12:25 +0530

    Test

--- src/main/java/com/spe/calculator/App.java
+++ src/main/java/com/spe/calculator/App.java
@@ -7,9 +7,6 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean running = true;
-
-
-
         System.out.println("Welcome to the Scientific Calculator");
         System.out.println("===================================");
 
